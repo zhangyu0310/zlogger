0772b9e853fcc17f23c1197adb189a4b892db3a4
Yu Zhang <505039426@qq.com>
2021-04-30 19:29:39 +0800

    Fix some bugs & make new feature
    1. Add Mutex to protect concurrency safety
    2. Add default logger for easy to use.

--- zlogger.go
+++ zlogger.go
@@ -1,3 +1,8 @@
+// Package zlogger is a go log package
+/*
+   Auto print file name & line at call point.
+   Can split log file automatically. (Prevent a single log file from being too large)
+*/
 package zlogger
 
 import (
@@ -5,8 +10,12 @@ import (
 	"os"
 	"runtime"
 	"strconv"
+	"sync"
 	"time"
 )
+
+// defaultLogger is a default logger for sample function.
+var defaultLogger = New("./", "zlogger", true)
 
 // Logger contain log of go & file handler.
 // Use logger.xxx() to log & set right prefix.
@@ -16,13 +25,15 @@ type Logger struct {
 	Path     string      // The path of Logger
 	Name     string      // The name of Logger without day
 	FileName string      // The name of Logger with day info
+	mutex    sync.Mutex  // Mutex for logger (Prefix order & update file safe)
 }
 
+// New create a new logger handler.
 // @path: dir of logs.
 // @name: prefix of logs.
 // Log file name just have year-month-day
 // Time of logs record is microseconds.
-func New(path string, name string) *Logger {
+func New(path, name string, autoUpdate bool) *Logger {
 	l := Logger{
 		Path: path,
 		Name: name,
@@ -36,7 +47,7 @@ func New(path string, name string) *Logger {
 		return nil
 	}
 	l.logger = log.New(l.file, "", log.LstdFlags|log.Lmicroseconds|log.Lmsgprefix)
-	if l.logger != nil {
+	if l.logger != nil && autoUpdate {
 		go func() {
 			// Check time and update logger file.
 			for true {
@@ -53,7 +64,10 @@ func New(path string, name string) *Logger {
 	return &l
 }
 
+// UpdateLoggerFile update the log file name. (Date suffix)
 func UpdateLoggerFile(logger *Logger) error {
+	logger.mutex.Lock()
+	defer logger.mutex.Unlock()
 	logger.FileName = logger.Name + "_" + time.Now().Format("2006-01-02")
 	filePath := logger.Path + "/" + logger.FileName
 	// Create new file handler & new logger
@@ -72,9 +86,9 @@ func UpdateLoggerFile(logger *Logger) error {
 	return nil
 }
 
-// GetFileAndLinePrefix get file name & line of call function.
-func GetFileAndLinePrefix() string {
-	_, file, line, ok := runtime.Caller(2)
+// getFileAndLinePrefix get file name & line of call function.
+func getFileAndLinePrefix(depth int) string {
+	_, file, line, ok := runtime.Caller(depth)
 	if !ok {
 		file = "???"
 		line = 0
@@ -92,37 +106,97 @@ func GetFileAndLinePrefix() string {
 }
 
 func (logger *Logger) Info(msg ...interface{}) {
-	prefix := GetFileAndLinePrefix()
+	logger.InfoN(3, msg...)
+}
+
+func (logger *Logger) InfoN(n int, msg ...interface{}) {
+	logger.mutex.Lock()
+	defer logger.mutex.Unlock()
+	prefix := getFileAndLinePrefix(n)
 	logger.logger.SetPrefix(prefix + "[INFO] ")
 	logger.logger.Println(msg...)
 }
 
 func (logger *Logger) Debug(msg ...interface{}) {
-	prefix := GetFileAndLinePrefix()
+	logger.DebugN(3, msg...)
+}
+
+func (logger *Logger) DebugN(n int, msg ...interface{}) {
+	logger.mutex.Lock()
+	defer logger.mutex.Unlock()
+	prefix := getFileAndLinePrefix(n)
 	logger.logger.SetPrefix(prefix + "[DEBUG] ")
 	logger.logger.Println(msg...)
 }
 
 func (logger *Logger) Warn(msg ...interface{}) {
-	prefix := GetFileAndLinePrefix()
+	logger.WarnN(3, msg...)
+}
+
+func (logger *Logger) WarnN(n int, msg ...interface{}) {
+	logger.mutex.Lock()
+	defer logger.mutex.Unlock()
+	prefix := getFileAndLinePrefix(n)
 	logger.logger.SetPrefix(prefix + "[WARN] ")
 	logger.logger.Println(msg...)
 }
 
 func (logger *Logger) Error(msg ...interface{}) {
-	prefix := GetFileAndLinePrefix()
+	logger.ErrorN(3, msg...)
+}
+
+func (logger *Logger) ErrorN(n int, msg ...interface{}) {
+	logger.mutex.Lock()
+	defer logger.mutex.Unlock()
+	prefix := getFileAndLinePrefix(n)
 	logger.logger.SetPrefix(prefix + "[ERROR] ")
 	logger.logger.Println(msg...)
 }
 
 func (logger *Logger) Fatal(msg ...interface{}) {
-	prefix := GetFileAndLinePrefix()
+	logger.FatalN(3, msg...)
+}
+
+func (logger *Logger) FatalN(n int, msg ...interface{}) {
+	logger.mutex.Lock()
+	defer logger.mutex.Unlock()
+	prefix := getFileAndLinePrefix(n)
 	logger.logger.SetPrefix(prefix + "[FATAL] ")
 	logger.logger.Fatalln(msg...)
 }
 
 func (logger *Logger) Panic(msg ...interface{}) {
-	prefix := GetFileAndLinePrefix()
+	logger.PanicN(3, msg...)
+}
+
+func (logger *Logger) PanicN(n int, msg ...interface{}) {
+	logger.mutex.Lock()
+	defer logger.mutex.Unlock()
+	prefix := getFileAndLinePrefix(n)
 	logger.logger.SetPrefix(prefix + "[PANIC] ")
 	logger.logger.Panicln(msg...)
+}
+
+func Info(msg ...interface{}) {
+	defaultLogger.InfoN(3, msg...)
+}
+
+func Debug(msg ...interface{}) {
+	defaultLogger.DebugN(3, msg...)
+}
+
+func Warn(msg ...interface{}) {
+	defaultLogger.WarnN(3, msg...)
+}
+
+func Error(msg ...interface{}) {
+	defaultLogger.ErrorN(3, msg...)
+}
+
+func Fatal(msg ...interface{}) {
+	defaultLogger.FatalN(3, msg...)
+}
+
+func Panic(msg ...interface{}) {
+	defaultLogger.PanicN(3, msg...)
 }
---
+++ zlogger_test.go
@@ -0,0 +1,139 @@
+package zlogger
+
+import (
+	"bufio"
+	"os"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestMultiOpenAndWrite(t *testing.T) {
+	l1 := New("./", "zlogger", false)
+	l2 := New("./", "zlogger", false)
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go func() {
+		for i := 0; i < 100; i++ {
+			l1.Info("Info l1", i)
+			l1.Debug("Debug l1", i)
+			l1.Warn("Warn l1", i)
+			l1.Error("Error l1", i)
+		}
+		wg.Done()
+	}()
+	go func() {
+		for i := 0; i < 100; i++ {
+			l2.Info("Info l2", i)
+			l2.Debug("Debug l2", i)
+			l2.Warn("Warn l2", i)
+			l2.Error("Error l2", i)
+		}
+		wg.Done()
+	}()
+	go func() {
+		for i := 0; i < 100; i++ {
+			Info("Info default", i)
+			Debug("Debug default", i)
+			Warn("Warn default", i)
+			Error("Error default", i)
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+}
+
+func TestFatal(t *testing.T) {
+	l1 := New("./", "zlogger", false)
+	l1.Fatal("Fatal l1")
+}
+
+func TestDefaultFatal(t *testing.T) {
+	Fatal("Fatal default")
+}
+
+func TestPanic(t *testing.T) {
+	l1 := New("./", "zlogger", false)
+	l1.Panic("Panic l1")
+}
+
+func TestDefaultPanic(t *testing.T) {
+	Panic("Panic default")
+}
+
+func TestUpdateLoggerFile(t *testing.T) {
+	l1 := New("./", "TestUpdateLoggerFile", false)
+	var wg sync.WaitGroup
+	wg.Add(5)
+	begin := time.Now().UnixNano()
+	go func() {
+		for i := 0; i < 10000; i++ {
+			l1.Info("Info l1", i)
+		}
+		wg.Done()
+	}()
+	go func() {
+		for i := 0; i < 10000; i++ {
+			l1.Debug("Debug l1", i)
+		}
+		wg.Done()
+	}()
+	go func() {
+		for i := 0; i < 10000; i++ {
+			l1.Warn("Warn l1", i)
+		}
+		wg.Done()
+	}()
+	go func() {
+		for i := 0; i < 10000; i++ {
+			l1.Error("Error l1", i)
+		}
+		wg.Done()
+	}()
+	go func() {
+		for i := 0; i < 10000; i++ {
+			err := UpdateLoggerFile(l1)
+			if err != nil {
+				t.Error("UpdateLoggerFile failed.", err)
+			}
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+	end := time.Now().UnixNano()
+	t.Log("Time cost:", end - begin)
+	//file, err := ioutil.ReadFile(l1.FileName)
+	//if err != nil {
+	//	t.Log("Check function ReadFile failed.", err)
+	//}
+	f, _ := os.Open(l1.Path + l1.FileName)
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			t.Log("File close failed.", err)
+		}
+	}(f)
+	reader := bufio.NewReader(f)
+	for {
+		line, _, err := reader.ReadLine()
+		lineStr := string(line)
+		if strings.Contains(lineStr, "DEBUG") {
+			strings.Contains(lineStr, "Debug")
+		} else if strings.Contains(lineStr, "INFO") {
+			strings.Contains(lineStr, "Info")
+		} else if strings.Contains(lineStr, "WARN") {
+			strings.Contains(lineStr, "Warn")
+		} else if strings.Contains(lineStr, "ERROR") {
+			strings.Contains(lineStr, "Error")
+		} else if lineStr == "" {
+
+		} else {
+			t.Error("String Contains error")
+		}
+		if err != nil {
+			break
+		}
+	}
+	t.Log("Check success.")
+}
